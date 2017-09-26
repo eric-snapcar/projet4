da2ae335e1eae377efad98b644f2f6775c76913a
Eric Hth <eric_hong_2000@yahoo.fr>
2017-09-27 00:22:21 +0200

    mnist MNIST original OK

--- tp2/tp2.py
+++ tp2/tp2.py
@@ -2,6 +2,4 @@ from sklearn.datasets import fetch_mldata
 mnist = fetch_mldata('MNIST original')
 # Le dataset principal qui contient toutes les images
 print mnist.data.shape
-
-# Le vecteur d'annotations associé au dataset (nombre entre 0 et 9)
-print mnist.target.shape
+print mnist.target
