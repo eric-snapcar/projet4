f73ee29f66aa2a0f69b62a8092a4bea608a362f6
Eric Hth <eric_hong_2000@yahoo.fr>
2017-09-28 02:57:05 +0200

    activity_loyer clean( ) clean_( ) DataFrame dropna() house_data house_data.price.notnull()

--- activite_loyer/activite_loyer.py
+++ activite_loyer/activite_loyer.py
@@ -1,20 +1,28 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-# On charge le dataset
 
+def isNaN(num):
+    return num != num
 def plot( house_data):
     plt.plot(house_data['price'], house_data['surface'], 'ro', markersize=4)
     plt.show()
     return;
-def clean( house_data ):
+def print_( house_data ):
     for index, row in house_data.iterrows():
         print row["price"], row["surface"], row["arrondissement"]
     return;
+def clean( house_data ):
+    return house_data[house_data.price.notnull() & house_data.surface.notnull() & house_data.arrondissement.notnull()];
+def clean_( house_data ):
+    return house_data.dropna();
 
 house_data_raw = pd.read_csv('house_data.csv')
-clean(house_data_raw)
-#plot (house_data_raw)
+house_data_raw_cleant = clean(house_data_raw)
+house_data_raw_cleant_ = clean_(house_data_raw)
+print(house_data_raw.shape)
+print(house_data_raw_cleant.shape)
+print(house_data_raw_cleant_.shape)
 
 """
 plt.plot(house_data_raw['price'], house_data_raw['surface'], 'ro', markersize=4)
