eae72ad8dd10f5da3e427360dfd8bda6b7f429c4
Eric Hth <eric_hong_2000@yahoo.fr>
2017-09-28 02:40:30 +0200

    clean( ) for index rox house_date iterrows() print

--- activite_loyer/activite_loyer.py
+++ activite_loyer/activite_loyer.py
@@ -3,14 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # On charge le dataset
 
-def plot( house_data_raw ):
-    plt.plot(house_data_raw['price'], house_data_raw['surface'], 'ro', markersize=4)
+def plot( house_data):
+    plt.plot(house_data['price'], house_data['surface'], 'ro', markersize=4)
     plt.show()
     return;
-
+def clean( house_data ):
+    for index, row in house_data.iterrows():
+        print row["price"], row["surface"], row["arrondissement"]
+    return;
 
 house_data_raw = pd.read_csv('house_data.csv')
-plot (house_data_raw)
+clean(house_data_raw)
+#plot (house_data_raw)
 
 """
 plt.plot(house_data_raw['price'], house_data_raw['surface'], 'ro', markersize=4)
