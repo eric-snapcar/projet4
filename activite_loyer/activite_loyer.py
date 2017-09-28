963fb920c309e54c121e96302c338beeb90b2a1e
Eric Hth <eric_hong_2000@yahoo.fr>
2017-09-28 13:21:34 +0200

    activite_loyer house_data_1 house_data_raw ...

--- activite_loyer/activite_loyer.py
+++ activite_loyer/activite_loyer.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 def plot( house_data):
     plt.plot(house_data['price'], house_data['surface'], 'ro', markersize=4)
@@ -10,20 +11,21 @@ def print_( house_data ):
     for index, row in house_data.iterrows():
         print row["price"], row["surface"], row["arrondissement"]
     return;
-def clean( house_data ):
+def clean_old( house_data ):
     return house_data[house_data.price.notnull() & house_data.surface.notnull() & house_data.arrondissement.notnull()];
-def clean_( house_data ):
+def clean( house_data ):
     return house_data.dropna();
 
 house_data_raw = pd.read_csv('house_data.csv')
-house_data_raw_cleant = clean(house_data_raw)
-house_data_raw_cleant_ = clean_(house_data_raw)
-print(house_data_raw.shape)
-print(house_data_raw_cleant.shape)
-print(house_data_raw_cleant_.shape)
+house_data_raw_clean = clean(house_data_raw)
+house_data_1 = house_data_raw_clean[['price','surface']]
+house_data_1_train, house_data_1_test = train_test_split(house_data_1, train_size=0.8)
+print(house_data_1_train)
+print(house_data_1_test)
 
-house_data_1 = house_data_raw[['price','surface']]
-print(house_data_1)
+
+
+
 """
 plt.plot(house_data_raw['price'], house_data_raw['surface'], 'ro', markersize=4)
 plt.show()
