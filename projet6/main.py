a2d46792202d32c4859d8c583421e021fa496ca4
Eric Hth <eric@snapcar.com>
2018-03-11 16:59:41 +0100

    main.py data() train test

--- projet6/main.py
+++ projet6/main.py
@@ -7,63 +7,16 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import metrics
 from sklearn.linear_model import SGDClassifier
-categories = ['alt.atheism', 'soc.religion.christian',
-              'comp.graphics', 'sci.med']
-twenty_train = fetch_20newsgroups(subset='train',
-                  categories=categories, shuffle=True, random_state=42)
-"""
-print(len(twenty_train.data))
-print(twenty_train.data[0])
-print(twenty_train.target[0])
-print(twenty_train.target_names)
 
-count_vect = CountVectorizer()
-
-X_train_counts = count_vect.fit_transform(twenty_train.data)
-print(X_train_counts.shape)
-print(count_vect.vocabulary_.get(u'algorithm'))
-print(count_vect.vocabulary_.get(u'hth'))
-
-
-
-tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
-X_train_tf = tf_transformer.transform(X_train_counts)
-X_train_tf.shape
-
-
-print(X_train_tf.shape)
-
-
-clf = MultinomialNB().fit(X_train_tf, twenty_train.target)
-
-
-docs_new = ['God is love', 'OpenGL on the GPU is fast']
-X_new_counts = count_vect.transform(docs_new)
-X_new_tfidf = tf_transformer.transform(X_new_counts)
-
-predicted = clf.predict(X_new_tfidf)
-
-for doc, category in zip(docs_new, predicted):
-    print('%r => %s' % (doc, twenty_train.target_names[category]))
-
-
-
-text_clf = Pipeline([('vect', CountVectorizer()),
-                     ('tfidf', TfidfTransformer()),
-                     ('clf', MultinomialNB()),
-])
-text_clf.fit(twenty_train.data, twenty_train.target)
-
-twenty_test = fetch_20newsgroups(subset='test',
-    categories=categories, shuffle=True, random_state=42)
-docs_test = twenty_test.data
-predicted = text_clf.predict(docs_test)
-np.mean(predicted == twenty_test.target)
- """
-
-twenty_test = fetch_20newsgroups(subset='test',
-    categories=categories, shuffle=True, random_state=42)
-docs_test = twenty_test.data
+def data():
+    categories = ['alt.atheism', 'soc.religion.christian',
+                  'comp.graphics', 'sci.med']
+    twenty_train = fetch_20newsgroups(subset='train',
+                      categories=categories, shuffle=True, random_state=42)
+    twenty_test = fetch_20newsgroups(subset='test',
+        categories=categories, shuffle=True, random_state=42)
+    return twenty_train, twenty_test
+twenty_train, twenty_test = data()
 text_clf = Pipeline([('vect', CountVectorizer()),
                      ('tfidf', TfidfTransformer()),
                      ('clf', SGDClassifier(loss='hinge', penalty='l2',
@@ -71,10 +24,7 @@ text_clf = Pipeline([('vect', CountVectorizer()),
                                            max_iter=5, tol=None)),
 ])
 text_clf.fit(twenty_train.data, twenty_train.target)
-
-predicted = text_clf.predict(docs_test)
-np.mean(predicted == twenty_test.target)
-
+predicted = text_clf.predict(twenty_test.data)
 
 print(metrics.classification_report(twenty_test.target, predicted,
     target_names=twenty_test.target_names))
